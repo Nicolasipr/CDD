aa0bcc85ec43825c2da5fea55a0cc59dd0bc58fd
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2019-08-29 00:19:45 -0400

    Handling **argv input
    Getting the argv input, parse it to long double and then save them into a vector of pairs

--- NewtonRaphson/programa.cpp
+++ NewtonRaphson/programa.cpp
@@ -1,13 +1,86 @@
 #include <bits/stdc++.h>
 
+// creates a ld shorcut for long double
 typedef long double ld;
+
+
+// define maximum and minimum values values to Newton Raphson iterarions
 #define N 100
-#define MAX_ERROR 0.001
+#define MAX_ERROR 10e-10 // if error is less than 10^10 stop
+#define MAX_ITERATOR 20  // I don't want to exceed a maximum of 20 itirations
 
 using namespace std;
 
-// vector < vector <float>> Matrix(N, vector<float>(N, 0));
+// GLOBAL VARIABLES
+string coefNumber = "",
+       expNumber;
 vector< pair< ld, ld > > polynomium(N, pair<ld, ld>(0,0)); // first = coef and second = exp
+int pIndex = 0;
+
+/*
+Functions:
+String to Long Double
+Power Calculator
+Function evaluation 
+Derivattive Function Evaluation
+
+*/
+
+void stringToLongDouble( int argc, char** argv){
+  cout << endl << endl; 
+
+    bool exponent = false;
+    int j = 0;
+
+    for(int i = 1; i < argc; ++i){ // if inputs more " " polynomial strings
+
+      while(argv[i][j]){ // travels through " " input
+
+        if(argv[i][j] == 'x' || argv[i][j] == 'X'){ // checks for coef and exp changes
+          exponent = true;
+          // cout << "String Coef Number: " << coefNumber << endl;
+          // cout << "string Exp Number: " << expNumber << endl;
+          // num = stold(coefNumber);
+          polynomium[pIndex].first = stold(coefNumber); // adds coef to pair
+          coefNumber = ""; // reset string
+        }
+              
+
+        if(exponent){ // while exponent == true
+            if(argv[i][j] != '*' && argv[i][j] != 'x' && argv[i][j] != 'X'){
+              expNumber += argv[i][j];
+              // polynomium[pIndex].second = polynomium[pIndex].second; // add exponent to pair
+            }
+            if(argv[i][j] == '-' || argv[i][j] == '+'){
+              exponent = false; 
+              polynomium[pIndex].second = stold(expNumber); 
+              pIndex++;
+              expNumber = "";
+            }
+        }
+        else{
+          if(argv[i][j] ==  '-' ){ // if it finds a - multiply by -1
+            polynomium[pIndex].first = -1;
+            exponent = false;
+          }
+          else{
+            polynomium[pIndex].first = 1;
+            exponent = false;
+          }
+
+
+            coefNumber += argv[i][j];
+  
+        }
+        j++;
+      }
+      
+      exponent = false;
+      polynomium[pIndex].second = stold(expNumber);
+      j = 0;
+    }
+    
+}
 
 void powerCalc(){
 
@@ -19,47 +92,18 @@ void functionCalc(){
 
 }
 
+void NewtonRaphson(){
+
+}
+
 int main(int argc, char** argv){
 
-    
-    cout << endl << endl; 
-
-    int j = 0;
-    for(int i = 1; i < argc; ++i){
-        while(argv[i][j]){
-
-            cout << argv[i][j];
-
-            // argv[i][j] == '-' ? polynomium[i].first = -1: polynomium[i].first = 1;
-            if(argv[i][j] ==  '-' )
-                polynomium[i].first = -1;
-
-            cout << polynomium[i].first << endl;
-
-             if(argv[i][j] >= 48 && argv[i][j] <= 57){
-
-                if(argv[i][j - 1] != '*'){
-                       polynomium[i].first = argv[i][j] - '0';
-                }
-                else{
-                    polynomium[i].second = argv[i][j] - '0';
-                }
-                
-             
-
-            }
-        
-
-            j++;
-        }
-
-        
-       
-
-        cout << endl;
-        j = 0;
-    }
-
-
-     return 0;
+  stringToLongDouble(argc, argv);
+  polynomium.resize(pIndex + 1);
+  for(int i = 0; i < polynomium.size(); i++){
+    cout << " Polinomio "<< i + 1 << " :" << endl
+         << "\tcoef : " << polynomium[i].first << endl
+         << "\texp  : " << polynomium[i].second << endl;
+  }
+  return 0;
 }
