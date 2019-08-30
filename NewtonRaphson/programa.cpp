3719b8d84cf46dec51fad813088476a548afa1a1
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2019-08-30 03:18:09 -0400

    adding newton Raphson integration and precision of 64 digits

--- NewtonRaphson/programa.cpp
+++ NewtonRaphson/programa.cpp
@@ -13,8 +13,8 @@ typedef long double ld;
 
 // define maximum and minimum values values to Newton Raphson iterarions
 #define N 1000000
-#define MAX_ERROR 10e-10 // if error is less than 10^10 stop
-#define MAX_ITERATOR 20  // I don't want to exceed a maximum of 20 itirations
+#define MAX_ERROR 10e-15 // if error is less than 10^15 stop
+#define MAX_ITERATOR 50  // I don't want to exceed a maximum of 20 itirations
 
 using namespace std;
 
@@ -25,18 +25,21 @@ vector< pair< ld, ld > > polynomium(N, pair<ld, ld>(0,0)); // first = coef and s
 vector< pair< ld, ld > > derivate(N, pair<ld, ld>(0,0)); // first = coef and second = exp
 vector< ld > powers(N, 0); // first = coef and second = exp
 
+ld fx = 0,  // p(x)
+   dx = 0;  // p(x)' 
 
 int pIndex = 0;
 
 /*
 Functions:
+sortBySec
 String to Long Double
 Power Calculator
 Function evaluation 
 Derivattive Function Evaluation
 */
 
-bool sortbysec(const pair<int,int> &a, 
+bool sortBySec(const pair<int,int> &a, 
               const pair<int,int> &b){ 
     return (a.second > b.second); // helps to sort by segment of pair (exponent)
 } 
@@ -113,9 +116,6 @@ void stringToLongDouble( int argc, char** argv){
 
 void derivating(){  // It derivates plynomium p(x)
 
-	// ld exp_aux[pIndex],
-  //   coef_aux[pIndex],
-  //   aux = 0;
 
 	for(int i=0; i <= pIndex; i++){ 
 
@@ -125,14 +125,13 @@ void derivating(){  // It derivates plynomium p(x)
 
         if (derivate[i].second >= 1){   
           derivate[i].first *= derivate[i].second;
-          // coef_aux[i]= aux;
           derivate[i].second -= 1; 
         }
 		
 	  }
 	}
 	cout<<"la derivada es ";
-		for(int i=0;i< pIndex;i++){
+		for(int i=0; i < pIndex;i++){
 		  cout<< derivate[i].first <<"x**" << derivate[i].second;
 
 	    if( i + 1 < pIndex)
@@ -151,29 +150,75 @@ void powerCalc(ld xValue){
 
 }
 
-void functionCalc(){
+void fxCalc(){
+
+  for(int i = 0; i < polynomium.size(); i++){
+    fx += polynomium[i].first * powers[polynomium[i].second];
+    dx += derivate[i].first * powers[derivate[i].second];
+  }
 
 }
 
+// void dxFunCalc(){ // less work if commented 
+
+//     for(int i = 0; i < derivate.size(); i++){
+//     dx += derivate[i].first * powers[derivate[i].second];
+//   }
+
+// }
 void NewtonRaphson(){
+  ld x_zero = 0.5,
+     x_one = 100,
+     h = 0;
+  
+  powerCalc(x_zero);
+  fxCalc();
+  h = x_one - x_zero;
+
+  if(dx > MAX_ERROR){ //avoiding zero division
+    int i = 0;
+    while( abs(h) > MAX_ERROR && i < MAX_ITERATOR){
+          // cout << "p("<< x_zero << ") = " << fx << endl;
+          // cout << "p("<< x_zero << ")' = " << dx << endl;
+
+        // cout << x_zero << endl;
+        x_one = x_zero - fx/dx;
+        powerCalc(x_one);
+        fxCalc();
+        h = x_one - x_zero;
+        x_zero =  x_one;
+        i++;
+        if(dx < MAX_ERROR)
+          break;
+    }
+  }
+  else{
+    cout << "dx is near to zero, cannot divide it..." << endl;
+  }
+
+  cout << "X_zero root value is :" << x_zero << endl;
 
 }
 
 int main(int argc, char** argv){
   
   ios_base::sync_with_stdio(false);
+  cout.precision(64); 
+
   stringToLongDouble(argc, argv);
 
   polynomium.resize(pIndex + 1); // it resizes vector to actual length 
   derivate.resize(pIndex + 1);
 
-  sort(polynomium.begin(), polynomium.end(), sortbysec);
+  sort(polynomium.begin(), polynomium.end(), sortBySec); // sorts polynomium by exponent desc
+
   derivating();
 
   ld x = 2;
-  powerCalc(x);
-  for(int i = 0; i < powers.size(); i++) //works 
-    cout << "Value of "<< x <<"^  " << i << " = "<< powers[i] << endl; // it shows all k-th powers of x
+  // powerCalc(0.8); // precomputes all powers 
+
+  // for(int i = 0; i < powers.size(); i++) //works 
+  //   cout << "Value of "<< x <<"^  " << i << " = "<< powers[i] << endl; // it shows all k-th powers of x
 
 
   for(int i = 0; i < polynomium.size(); i++){
@@ -181,5 +226,11 @@ int main(int argc, char** argv){
          << "\tcoef : " << polynomium[i].first << endl
          << "\texp  : " << polynomium[i].second << endl;
   }
+  // fxCalc();
+  // dxFunCalc();
+  // cout << "p("<< x << ") = " << fx << endl;
+  // cout << "p("<< x << ")' = " << dx << endl;
+
+  NewtonRaphson();
   return 0;
 }
