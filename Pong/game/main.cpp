db4dbfa1c34155e488180d847f69a6005be1354b
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2019-10-29 23:54:17 -0300

    Adding refeshing time to 100ms
    100ms it's has been setted as a 'normal' gamepay experience

--- Pong/game/main.cpp
+++ Pong/game/main.cpp
@@ -3,13 +3,30 @@
 //
 
 #include <iostream>
+#include <ncurses.h>
+#include <chrono>
+#include <thread>
 #include "Board.h"
+#include "Player.h"
+#include "Ball.h"
 
 using namespace std;
 
 int main(){
+
     Board Game;
+    Player P1(1), P2(2);
+    int x = Game.getXPos(),
+    y = Game.getYPos();
+
+    do{
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        Game.showBoard();
+        Game.setBallPos(x++, y++);
+    }while(true);
     Game.showBoard();
 
+//    Game.onKey();
 
+    return 0;
 }
