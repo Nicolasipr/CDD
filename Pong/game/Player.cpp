d66e66b966d54294abf183e62f029250ffa6a24d
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2019-12-20 01:28:18 -0300

    In Player Constructor it's setted its UDP connection function before entering the game

--- Pong/game/Player.cpp
+++ Pong/game/Player.cpp
@@ -2,4 +2,107 @@
 // Created by nicolasipr on 15/10/2019.
 //
 
-#include "Player.h"
+#include "includes/Player.h"
+#include <iostream>
+//socket
+#include <bits/stdc++.h>
+#include <arpa/inet.h>
+// #include <sys/socket.h>
+#include <netinet/in.h> // socket struct
+#include <unistd.h>
+#include <netdb.h>
+
+#define BUFFER_SIZE 1024
+
+
+using namespace std;
+
+Player::Player(int newSide) {
+    playerSide = newSide;
+    connection();
+}
+Player::~Player() {
+    cout << "Player " << playerSide << " Has ben  destroyed!";
+//    closeConnection();
+}
+int Player::getPaddleYPos() {
+    return paddleYPos;
+}
+int Player::getPaddleXPos() {
+    return paddleXPos;
+}
+int Player::getPlayerSide(){
+    return playerSide;
+}
+char * Player::getPaddle() {
+    return paddle;
+}
+
+void Player::sendPosY(){
+
+
+}
+
+void Player::connection(){
+
+    hostent * record = gethostbyname(getAddress());
+
+    if(record == NULL){
+        cout <<"\n" <<  getAddress() <<" is unavailable";
+        exit(1);
+    }
+
+
+    in_addr * addressHost = (in_addr * )record->h_addr;
+    char* ip_address = inet_ntoa(* addressHost);
+
+    int port = getPort();
+    char c = controlInput();
+    const char * message = reinterpret_cast<const char(*)>(&c);
+
+    cout << "\nServer Name Address: " << getAddress() << endl
+         << "IPv4 Address: " << ip_address << endl
+         << "Port: " << port << endl
+         << "Message: " << message << endl;
+
+
+    struct sockaddr_in server; //IPV4
+    char buf[BUFFER_SIZE];
+
+    // udp connection
+
+    //  Socket
+    int sock = socket(AF_INET, SOCK_DGRAM, 0); // 0 means default, AF_NET = IPV4 & SOCK_DGRAM = TCP
+
+
+    if(sock < 0){
+        cout << "Error while opening socket";
+        exit(1);
+    }
+    // connection
+
+    memset(&server, 0, sizeof (server));
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    inet_pton(AF_INET, ip_address, &server.sin_addr);
+
+    unsigned int len;
+
+
+    sendto(sock, message, strlen(message) ,
+           MSG_CONFIRM, (const struct sockaddr *) &server,
+           sizeof(server));
+    cout << "message sent" << endl;
+
+    recvfrom(sock, (char *) buf, BUFFER_SIZE,
+                 MSG_WAITALL, (struct sockaddr *) &server,
+                 &len);
+
+    buf[len] = '\0';
+
+    printf("Server: %s\n", buf);
+
+    close(sock);
+}
--- Pong/game/includes/Player.h
+++ Pong/game/includes/Player.h
@@ -1,23 +1,42 @@
 //
 // Created by nicolasipr on 15/10/2019.
 //
-//#include "Board.cpp"
+
 #ifndef GAME_PLAYER_H
 #define GAME_PLAYER_H
 
-#include "Board.h"
 
-class Player: public Board {
+//#include "Board.h"
+class Game;
+#include "Game.h"
+
+class Player : public Game {
 
 private:
+    char name[32] = "192.168.0.1";
     int playerScore = 0;
-    int playerSide = 1; // 1 or two
-    char paddle[7] = "######";
-
+    int playerSide = 1; // 1 or 2
+    char paddle[7] = "123456";
+    int paddleYPos = 0; // centering players paddle.
+    int paddleXPos  = 0;
 
 public:
     Player(int newSide);
     ~Player();
+
+    void connection();
+
+    int getPaddleYPos();
+    void setPaddleYPos();
+
+    int getPaddleXPos();
+    int getPlayerSide();
+
+    char * getPaddle();
+    void sendPosY();
+
+
+
 };
 
 
